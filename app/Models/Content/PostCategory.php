c81fc98e25679273a2c76665d993baaeab1b2541
Amin Ataei <amin_ataei@live.com>
2025-01-27 10:33:00 +0330

    [feat]: - Added the ability to set a slug with a title

--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -4,11 +4,21 @@ namespace App\Models\Content;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class PostCategory extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory, Sluggable;
 
+    public function sluggable()
+{
+    return [
+        'slug' => [
+            'source' => 'name'
+        ]
+    ]
+}
     protected $table = 'post_categories';  // نام جدول
     protected $guarded = ['id'];  // فیلدهای محافظت شده
     protected $fillable = ['name', 'description', 'slug', 'image', 'status', 'tags'];
